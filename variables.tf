8fbcb75abca7799a6b53e8973a04ebe86f91e849
Ari Falkner <ari@open.store>
2022-07-07 16:31:00 -0400

    set all availability zones

--- variables.tf
+++ variables.tf
@@ -516,8 +516,8 @@ variable "security_group_use_name_prefix" {
   default     = true
 }
 
-variable "putin_khuylo" {
-  description = "Do you agree that Putin doesn't respect Ukrainian sovereignty and territorial integrity? More info: https://en.wikipedia.org/wiki/Putin_khuylo!"
-  type        = bool
-  default     = true
+variable "availability_zones" {
+  description = "The availability zones of the cluster"
+  type        = list(string)
+  default     = ["us-west-2a", "us-west-2b", "us-west-2c"]
 }
